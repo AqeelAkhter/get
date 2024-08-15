039e99e9fb04bbad01be32d5bad4eaf1830d8bcf
AqeelAkhter <97888681+AqeelAkhter@users.noreply.github.com>
2024-08-15 05:07:03 -0700

    test

---
+++ data.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/user");
+
+const app = express();
+const PORT = 4000;
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  email: { type: String, required: true },
+});
+
+const datamodal = mongoose.model("data", userSchema);
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/get", (req, res) => {
+  datamodal.find().then((resu) => {
+    res.json(resu);
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  });
+});
+
+app.post("/voice", (req, res) => {
+});
+
+const Start = async () => {
+  app.listen(PORT, () => {
+    console.log("App is running on port",);
+  });
+};
+
+Start();
+
+
+
+
+
+
