ca7bdf63cae793a664cf38b8a395c16d2cbea5b4
AqeelAkhter <97888681+AqeelAkhter@users.noreply.github.com>
2024-08-21 00:02:01 -0700

    update

--- data.js
+++ data.js
@@ -1,58 +1,58 @@
-require("dotenv").config({ path: "./config/config.env" });
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-const dbConnect = require("./config/db");
+// require("dotenv").config({ path: "./config/config.env" });
+// const express = require("express");
+// const cors = require("cors");
+// const morgan = require("morgan");
+// const mongoose = require("mongoose");
+// const dbConnect = require("./config/db");
 
-const app = express();
-app.use(morgan("tiny"));
-app.use(express.json());
+// const app = express();
+// app.use(morgan("tiny"));
+// app.use(express.json());
 
-const PORT = process.env.PORT || 4000;
+// const PORT = process.env.PORT || 4000;
 
-const corsOptions = {
-  origin: process.env.CLIENT_ORIGIN || '*',
-  optionsSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: process.env.CLIENT_ORIGIN || '*',
+//   optionsSuccessStatus: 200,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  email: { type: String, required: true },
-});
+// const userSchema = new mongoose.Schema({
+//   name: { type: String, required: true, unique: true },
+//   email: { type: String, required: true },
+// });
 
-// This model will use the "data" collection in the "user" database
-const DataModel = mongoose.model("data", userSchema);
+// // This model will use the "data" collection in the "user" database
+// const DataModel = mongoose.model("data", userSchema);
 
-app.get("/get", (req, res) => {
-  DataModel.find()
-    .then((resu) => {
-      res.json(resu);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Internal server error");
-    });
-});
+// app.get("/get", (req, res) => {
+//   DataModel.find()
+//     .then((resu) => {
+//       res.json(resu);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).send("Internal server error");
+//     });
+// });
 
-app.post("/voice", (req, res) => {
-  // Implement your logic here
-});
+// app.post("/voice", (req, res) => {
+//   // Implement your logic here
+// });
 
-const start = async () => {
-  try {
-    await dbConnect();
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
-  } catch (err) {
-    console.error('Failed to start server:', err);
-  }
-};
+// const start = async () => {
+//   try {
+//     await dbConnect();
+//     app.listen(PORT, () => {
+//       console.log(`Server running on http://localhost:${PORT}`);
+//     });
+//   } catch (err) {
+//     console.error('Failed to start server:', err);
+//   }
+// };
 
-start();
+// start();
 
 
 // require("dotenv").config({path:"./config/config.env"});
@@ -118,47 +118,47 @@ start();
 
 
 
-// const express = require("express");
-// const cors = require("cors");
-// const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 
-// // mongoose.connect("mongodb+srv://akeelakhtar71:yeBKEKB8sJdSp8gK@cluster0.047ye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-// mongoose.connect("mongodb://localhost:27017/user");
+// mongoose.connect("mongodb+srv://akeelakhtar71:yeBKEKB8sJdSp8gK@cluster0.047ye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect("mongodb://localhost:27017/user");
 
-// const app = express();
-// const PORT = process.env.PORT || 4000;
+const app = express();
+const PORT = process.env.PORT || 4000;
 
-// const userSchema = new mongoose.Schema({
-//   name: { type: String, required: true, unique: true },
-//   email: { type: String, required: true },
-// });
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  email: { type: String, required: true },
+});
 
-// const datamodal = mongoose.model("data", userSchema);
+const datamodal = mongoose.model("data", userSchema);
 
-// app.use(express.json());
-// app.use(cors());
+app.use(express.json());
+app.use(cors());
 
-// app.get("/get", (req, res) => {
-//   datamodal.find().then((resu) => {
-//     res.json(resu);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//     res.status(500).send("Internal server Error");
-//   });
-// });
+app.get("/get", (req, res) => {
+  datamodal.find().then((resu) => {
+    res.json(resu);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).send("Internal server Error");
+  });
+});
 
-// app.post("/voice", (req, res) => {
-// });
+app.post("/voice", (req, res) => {
+});
 
-// const Start = async () => {
-//   app.listen(PORT, () => {
-//     console.log("App is running on port",);
-//   });
-// };
+const Start = async () => {
+  app.listen(PORT, () => {
+    console.log("App is running on port",);
+  });
+};
 
-// Start();
+Start();
 
 
 
