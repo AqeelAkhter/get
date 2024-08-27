8d70eb81f970f50b198a8e5bd42329bb976cebb7
AqeelAkhter <97888681+AqeelAkhter@users.noreply.github.com>
2024-08-26 22:21:44 -0700

    update

--- data.js
+++ data.js
@@ -118,13 +118,60 @@
 
 
 
+// const express = require("express");
+// const cors = require("cors");
+// const mongoose = require("mongoose");
+
+
+// //mongoose.connect("mongodb+srv://akeelakhtar71:akeel123@cluster0.047ye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+// mongoose.connect("mongodb://localhost:27017/user");
+
+// const app = express();
+// const PORT = process.env.PORT || 4000;
+
+// const userSchema = new mongoose.Schema({
+//   name: { type: String, required: true, unique: true },
+//   email: { type: String, required: true },
+// });
+
+// const datamodal = mongoose.model("data", userSchema);
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.get("/get", (req, res) => {
+//   datamodal.find().then((resu) => {
+//     res.json(resu);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     res.status(500).send("Internal server Error");
+//   });
+// });
+
+// app.post("/voice", (req, res) => {
+// });
+
+// const Start = async () => {
+//   app.listen(PORT, () => {
+//     console.log("App is running on port",);
+//   });
+// };
+
+// Start();
+
+
+
+
+
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-
-//mongoose.connect("mongodb+srv://akeelakhtar71:akeel123@cluster0.047ye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-mongoose.connect("mongodb://localhost:27017/user");
+mongoose.connect("mongodb://localhost:27017/user", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -140,28 +187,26 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/get", (req, res) => {
-  datamodal.find().then((resu) => {
-    res.json(resu);
-  })
-  .catch((err) => {
-    console.log(err);
-    res.status(500).send("Internal server Error");
-  });
+  datamodal.find()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+    });
 });
 
 app.post("/voice", (req, res) => {
+  // Your POST logic here
+  res.send("POST request received");
 });
 
 const Start = async () => {
   app.listen(PORT, () => {
-    console.log("App is running on port",);
+    console.log("App is running on port", PORT);
   });
 };
 
 Start();
-
-
-
-
-
 
